7e01cb8d610c11a29434af088a6d2b03f535df6e
textual-javascript <40214045+textual-javascript@users.noreply.github.com>
2018-06-17 12:35:39 +0200

    Add files via upload

--- apps/todo-list.js
+++ apps/todo-list.js
@@ -1,4 +1,8 @@
 var todos = [];
+var canAddItems = false;
+var canRemoveItems = false;
+var canShowItems = false;
+var canShowCommands = false
 var input = $("input[type='text']");
 
 printOut("Welcome to my todo-list app! If you don't know commands, type help.")
@@ -23,44 +27,55 @@ input.keypress(function(event){
 });
 
 function listTodos(){
-   if(todos.length !== 0){
+   canShowItems = true
+   if(todos.length !== 0 && canShowItems === true){
 	printOut("Here are your things to do:")
 	todos.forEach(function(todo, i){
     printOut(i + ": " + todo);  
- });
+  });
+    canShowItems = false
 }
-  else{
-	printOut("Your list is empty!")  
- }
+  else if(todos.length === 0 && canShowItems === true){
+	printOut("Your list is empty!") 
+    canShowItems = false	
+  }
 }
 
 function addTodo(){
     printOut("Type thing you want to add.");
+	canAddItems = true;
 	input.keypress(function(event){
-     if(event.which === 13){
+     if(event.which === 13 && canAddItems === true){
       var newTodo = command
 	  todos.push(" " + newTodo);
-	  printOut("Item added successfully");	  
+	  printOut("Item added successfully");
+      canAddItems = false	  
 	}
   })
 }
  
 function deleteTodo(){
 	printOut("What item do you want to remove? (Type its index)");
+	canRemoveItems = true
 	input.keypress(function(event){
-	if(event.which === 13){
+	if(event.which === 13 && canRemoveItems == true){
       var index = command
 	  todos.splice(index, 1)
 	  printOut("Item removed successfully");
+	  canRemoveItems = false
 	}
   })
 }
 
 function viewCommands(){
-	printOut("Here's a full list of commands:");
-	printOut("new - adds new item to the list.");
-	printOut("list - views list of things to do.");
-	printOut("delete - deletes item from the list.");
-	printOut("help - shows list of commands.");
-	printOut("close - closes the application");
+	canShowCommands = true
+	if(canShowCommands === true){
+	  printOut("Here's a full list of commands:");
+	  printOut("new - adds new item to the list.");
+	  printOut("list - views list of things to do.");
+	  printOut("delete - deletes item from the list.");
+	  printOut("help - shows list of commands.");
+	  printOut("close - closes the application");
+      canShowCommands = false	  
+	}
 }
