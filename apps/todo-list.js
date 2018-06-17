871dd7907a6656ac16bea0d316052582eaabc6b7
textual-javascript <40214045+textual-javascript@users.noreply.github.com>
2018-06-17 11:56:25 +0200

    Add files via upload

--- apps/todo-list.js
+++ apps/todo-list.js
@@ -4,9 +4,56 @@ var input = $("input[type='text']");
 printOut("Welcome to my todo-list app! If you don't know commands, type help.")
 
 input.keypress(function(event){
-	if(command == "new"){
-	  var newTodo = printOut("Type thing you want to add.");
-	  todos.push(" " + newTodo);
-	  printOut("Item added successfully");
-	}
-})
+ if(event.which === 13){
+  if(command === "list"){
+     listTodos();
+  }
+
+  else if(command === "new"){
+     addTodo();
+  }
+  
+  else if(command === "delete"){
+	 deleteTodo();
+  }
+  else if(command === "help"){
+     viewCommands();
+  }
+  else{
+	 printOut("Invalid command. If you don't know commands, type help.");
+  }	 
+ }
+});
+
+function listTodos(){
+   if(todos.length !== 0){
+	printOut("Here are your things to do:")
+	todos.forEach(function(todo, i){
+    printOut(i + ": " + todo);  
+ });
+}
+  else{
+	printOut("Your list is empty!")  
+ }
+}
+
+function addTodo(){
+	var newTodo= prompt("Type thing you want to add.");
+	todos.push(" " + newTodo);
+	printOut("Item added successfully");
+}
+
+function deleteTodo(){
+	var index= prompt("What item do you want to remove? (Type its index)");
+	todos.splice(index, 1)
+	printOut("Item removed successfully");
+}
+
+function viewCommands(){
+	printOut("Here's a full list of commands:");
+	printOut("new - adds new item to the list.");
+	printOut("list - views list of things to do.");
+	printOut("delete - deletes item from the list.");
+	printOut("help - shows list of commands.");
+	printOut("close - closes the application");
+}
