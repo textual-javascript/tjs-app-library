777cfefc2fa983e75765157ae511ea10a1c2783b
textual-javascript <40214045+textual-javascript@users.noreply.github.com>
2018-06-18 13:04:56 +0200

    Add files via upload

--- apps/todo-list.js
+++ apps/todo-list.js
@@ -1,92 +1,121 @@
 var todos = [];
 var forbiddenTodos = ["new", "list", "delete", "help", "close"];
-var canAddItems = false;
-var canRemoveItems = false;
-var canShowItems = false;
-var canShowCommands = false;
-var stopExec = false;
+var canAddItems = true;
+var canRemoveItems = true;
+var canShowItems = true;
+var canShowCommands = true;
+var canStopApp = true;
+var stopExec = true;
 var input = $("input[type='text']");
 
 printOut("Welcome to my todo-list app! If you don't know commands, type help.")
 
 input.keypress(function(event){
- if(event.which === 13){
+ if(event.which === 13 && stopExec === false){
   
-  if(command === "list" && stopExec === false){
+  if(command === "list" && canShowItems === true){
      listTodos();
   }
 
-  else if(command === "new" && stopExec === false){
+  else if(command === "new" && canAddItems === true){
      addTodo();
   }
   
-  else if(command === "delete" && stopExec === false){
+  else if(command === "delete" && canRemoveItems == true){
 	 deleteTodo();
-	 canRemoveItems = false
   }
   
-  else if(command === "help" && stopExec === false){
+  else if(command === "help" && canShowCommands == true){
      viewCommands();
   }
  
-  else if(command === "close" && stopExec === false){
+  else if(command === "close" && canStopApp = true){
 	 closeApp(); 
   }
  }
 });
 
 function listTodos(){
-   canShowItems = true
-   if(todos.length !== 0 && canShowItems === true){
+  canAddItems = false
+  canRemoveItems = false
+  canShowCommands = false
+  canStopApp = false
+  if(todos.length !== 0){
 	printOut("Here are your things to do:")
 	todos.forEach(function(todo, i){
     printOut(i + ": " + todo);  
   });
 }
-  else if(todos.length === 0 && canShowItems === true){
+  else if(todos.length === 0){
 	printOut("Your list is empty!") 	
   }
   printOut("Type command");
-  canShowItems = false
+  canAddItems = true
+  canShowItems = true
+  canRemoveItems = true
+  canShowCommands = true
+  canStopApp = true
 }
 
 function addTodo(){
+	canShowItems = false
+    canRemoveItems = false
+    canShowCommands = false
+    canStopApp = false
     printOut("Type thing you want to add.");
 	canAddItems = true;
 	input.keypress(function(event){
-     if(event.which === 13 && canAddItems === true){
+     if(event.which === 13){
 		var newTodo = command
 	    todos.push(" " + newTodo);
 	    printOut("Item added successfully");
 	 }		  
-	 canAddItems = false
+	 canAddItems = true
+     canShowItems = true
+     canRemoveItems = true
+     canShowCommands = true
+     canStopApp = true
   })
 }
  
 function deleteTodo(){
+    canShowItems = false
+    canAddItems = false
+    canShowCommands = false
+    canStopApp = false
 	printOut("What item do you want to remove? (Type its index)");
 	canRemoveItems = true
 	input.keypress(function(event){
-	if(event.which === 13 && canRemoveItems == true){
+	if(event.which === 13){
 	  canRemoveItems = false
       var index = command
 	  todos.splice(index, 1)
 	  printOut("Item removed successfully");
 	}
   })
+  	 canAddItems = true
+     canShowItems = true
+     canRemoveItems = true
+     canShowCommands = true
+     canStopApp = true
 }
 
 function viewCommands(){
-	canShowCommands = true
-	if(canShowCommands === true){
-	  canShowCommands = false
+	  canShowItems = false
+      canAddItems = false
+      canRemoveItems = false
+      canStopApp = false
 	  printOut("Here's a full list of commands:");
 	  printOut("new - adds new item to the list.");
 	  printOut("list - views list of things to do.");
 	  printOut("delete - deletes item from the list.");
 	  printOut("help - shows list of commands.");
-	  printOut("close - closes the application");	  
-	}
+	  printOut("close - closes the application");
+  	  canAddItems = true
+      canShowItems = true
+      canRemoveItems = true
+      canShowCommands = true
+      canStopApp = true	  
 }
 
 function closeApp(){
