1f3dd7a2d545f2ed35b4d7c22aae0869344055af
textual-javascript <40214045+textual-javascript@users.noreply.github.com>
2018-06-19 10:58:38 +0200

    Add files via upload

--- apps/penalty-shootout.js
+++ apps/penalty-shootout.js
@@ -1,6 +1,6 @@
 var instructions = ["Alright, rules are very simple.", "You have to pick number from 1 to 48, to shoot.", "In the meantime, computer also picks number from the same range.", "If difference between computer's number, and your number is greater than 2, you score goal.", "But if not, then goalkeeper saves your shot.", "Computer's takes penalties in the exact same way, as you do.", "The first one to score 10 points, wins!", "Type ok, when you'll be ready"]
-console.log(instructions.length);
-console.log(instructions[7]);
+var waitForOk = false
+
 printOut("Welcome to penalty shootout! Type start.")
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
@@ -11,10 +11,12 @@ $("input[type='text']").keypress(function(event){
 })
 	
 function game(){
-  for(var i = 0; i < 7; i++){
+  for(var i = 0; i < 8; i++){
 	  printOut(instructions[i])
+	  waitForOk = true
   }
-  if(command === "ok"){
+  if(command === "ok" && waitForOk === true){
 	  printOut("Ok then, let's go!");
+	  waitForOk = false
   }
 }
