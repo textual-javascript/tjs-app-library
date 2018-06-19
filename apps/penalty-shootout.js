b9aec5644bf845a427fbfb6df162fb1dc8028547
textual-javascript <40214045+textual-javascript@users.noreply.github.com>
2018-06-19 07:19:53 +0200

    Add files via upload

--- apps/penalty-shootout.js
+++ apps/penalty-shootout.js
@@ -1,23 +1,19 @@
 var input = $("input[type='text']")
 var command = ""
-var printRules = setInterval(explainRules, 2000);
-var instructions = ["Alright, rules are very simple.", "You have to pick number from 1 to 48, to shoot.", "In the meantime, computer also picks number from the same range.", "If difference between computer's number, and your number is greater than 2, you score goal.", "But if not, then goalkeeper saves your shot.", "Computer's takes penalties in the exact same way, as you do.", "The first one to score 10 points, wins!"]
+var instructions = ["Alright, rules are very simple.", "You have to pick number from 1 to 48, to shoot.", "In the meantime, computer also picks number from the same range.", "If difference between computer's number, and your number is greater than 2, you score goal.", "But if not, then goalkeeper saves your shot.", "Computer's takes penalties in the exact same way, as you do.", "The first one to score 10 points, wins!", "Press y, to play."]
 
-printOut("Welcome to penalty shootout! Type start, to play...")
+printOut("Welcome to penalty shootout! Type start.")
 input.keypress(function(event){
 	if(event.which === 13){
 		command = input.val();
 		if(command === "start"){
-			
+			explainRules()
 		}
 	}
 })
 	
 function explainRules(){
-	var index = 0
-	  printOut(instructions[index]);
-      index++		
-	  if(index === 6){
-		  
-	  }
+  for(var i = 0; i < instructions.length - 1; i++){
+	  printOut(instructions[i])
+  }
 }
